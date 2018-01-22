26e848ce9f91dfdd8cc5d2e527890e2e92f4616d
Felipe de Campos <felipe.campos.programador@gmail.com>
2018-01-22 16:19:14 -0200

    Authenticator Interface
    Created a Authenticator Interface

---
+++ External/Api/AuthenticatorInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\External\Api;
+
+
+/**
+ * Interface AuthenticatorInterface
+ *
+ * An interface to add additional authentication parameters to the
+ * request
+ *
+ * @package Application\External\Api
+ * @author Felipe de Campos <felipe.campos.programador@gmail.com>
+ */
+interface AuthenticatorInterface
+{
+
+    /**
+     * Modifies guzzle options to meet authentication requirements for specific API
+     *
+     * @param array $options
+     * @param mixed $data
+     * @return null
+     */
+    public function modifyOptions(array &$options, $data = null);
+
+}
